a490c86029c4f31024d58a80c572d69a5d850464
Micunymos2000 <149001337+micunymos@users.noreply.github.com>
2025-08-08 17:36:37 +0200

    Update and rename readme.cmd to start.cmd

--- start.cmd
+++ start.cmd
@@ -3,10 +3,10 @@ cls
 color 80
 echo Welcome to Micunymos Vista.
 echo ---------------------------
-echo Please use a console with default settings, to run Micunymos Vista, because all images are made with ASCII art.
+echo Please use a console with default settings, to run Micunymos Vista Basic, because all images are made with ASCII art.
 echo The default console font is Consolas, at the size of 16 pixels.
 echo ---------------------------
-echo IMPORTANT NOTE: Micunymos Vista Basic, and Professional are not compatible with Windows 11 stably,
+echo IMPORTANT NOTE: Micunymos Vista Home Basic, and Home Professional are not compatible with Windows 11 completely,
 echo                 due to visual differences between Windows 10, and 11.
 echo ---------------------------
 echo Do you wish to proceed to the installer?
@@ -16,4 +16,5 @@ if %errorlevel%==1 (
 	cd /d %~dp0
 	install.bat
 )
+
 if %errorlevel%==2 exit /b
